167fcc8bf449f537f37ba10acef06af61c9fc527
Aditya Narayan Shahi <adityan.shahi2014@gmail.com>
2018-02-07 11:41:21 +0530

    Added start page and highscore to FLappy bird

--- Flappy bird/main.py
+++ Flappy bird/main.py
@@ -3,16 +3,18 @@ import random
 import time
 
 pygame.init()
+pygame.font.init()
 # 23 24   52 623
 dw=400
 dh=600
 flag=0
 xe=0
 score =0
+highscore=0
 ye=0
 display=pygame.display.set_mode((dw,dh))
 thistime=-1
-color={'white':(255,255,255),'black':(0,0,0),'cyan':(0,125,0)}
+color={'white':(255,255,255),'black':(0,0,0),'yellow':(255,255,0),'cyan':(0,255,255),'red':(125,0,0),'green':(0,125,0),'darkgreen':(0,255,0),'darkred':(255,0,0)}
 
 pygame.display.set_caption('Flappy Bird')
 
@@ -43,7 +45,14 @@ def text_objects(text, font):
     return textSurface, textSurface.get_rect()
 
 def diedfunc():
+    global highscore
+    global score
+    if(score>highscore):
+        file=open("DO-NOT-EDIT.txt",'w')
+        file.write(str(score))
+        file.close()
     message_display("DIED")
+    
 
 def imgfunc(x,y):
     display.blit(img,(x,y))
@@ -62,6 +71,55 @@ def score_display(text):
     rect.center=(70,20)
     display.blit(surf,rect)
     pygame.display.update()
+
+def hscore_display(text):
+    texta =  pygame.font.Font('freesansbold.ttf',20)
+    surf,rect = text_objects(text,texta)
+    rect.center=(70,50)
+    display.blit(surf,rect)
+    pygame.display.update()
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(display, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            action()         
+    else:
+        pygame.draw.rect(display, ic,(x,y,w,h))
+    smallText = pygame.font.Font('freesansbold.ttf',20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    display.blit(textSurf, textRect)
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        display.fill(color['yellow'])
+        texta = pygame.font.SysFont("comicsansms",50)
+        surf,rect = text_objects("FLAPPY",texta)
+        rect.center = ((dw/2),100)
+        display.blit(surf,rect)
+        surf,rect = text_objects("BIRD",texta)
+        rect.center = ((dw/2),200)
+        display.blit(surf,rect)
+        surf,rect = text_objects("CLONE",texta)
+        rect.center = ((dw/2),300)
+        display.blit(surf,rect)
+
+        button("PLAY",50,450,100,50,color['green'],color['darkgreen'],gameloop)
+        button("QUIT",250,450,100,50,color['red'],color['darkred'],pygame.quit)
+
+        pygame.display.update()
+        clock.tick(15)
 
 def jump(x,y):
     global thistime
@@ -97,6 +155,7 @@ def jump(x,y):
         
         display.blit(bgimg,(0,0))
         score_display("Score: "+str(score))
+        hscore_display("High Score: "+str(highscore))
         if flag==1:
             xe-=5
             enemyfunc(xe,ye)
@@ -121,6 +180,11 @@ def gameloop():
     global flag
     he=200
     we=50
+    global highscore
+    file=open("DO-NOT-EDIT.txt",'r')
+    string=file.read()
+    file.close()
+    highscore=int(string)
     flag=-1
     timea=0
     xe=dw-100
@@ -139,8 +203,7 @@ def gameloop():
                 y=jump(x,y)
                 change=10
         if (y>(dh-15)):
-            print("NOW")
-            died=True
+            diedfunc()
         y+=change
         if flag==-1:
             if thistime>=100:
@@ -162,6 +225,7 @@ def gameloop():
         #display.fill(color['cyan'])
         display.blit(bgimg,(0,0))
         score_display("Score: "+str(score))
+        hscore_display("High Score: "+str(highscore))
         imgfunc(x,y)
         if flag==1:
             xe-=5
@@ -171,6 +235,7 @@ def gameloop():
             diedfunc()
         pygame.display.update()
         clock.tick(60)
+game_intro()
 gameloop()
 pygame.quit()
 quit()
